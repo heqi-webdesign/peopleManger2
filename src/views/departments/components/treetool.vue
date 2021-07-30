3f40d6581b5f695b0dbb4f3b5a6eccbdfb297bfa
wsy-gallery-github <873531126@qq.com>
2021-07-30 19:56:32 +0800

    departments---转化树形结构

---
+++ src/views/departments/components/treetool.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="mytree">
+    <el-row>
+      <el-col :span="20" class="left">
+        <i class="el-icon-office-building" v-if="item.isRoot === true"></i>
+        <i class="el-icon-s-promotion" v-else-if="item.pid === ''"></i>
+        <i class="el-icon-star-on" v-else></i>
+
+        {{ item.name }}</el-col
+      >
+      <el-col :span="4" class="right">
+        <span>负责人</span>
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            操作<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <template>
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!item.isRoot">查看部门</el-dropdown-item>
+              <el-dropdown-item v-if="!item.isRoot">删除部门</el-dropdown-item>
+            </template>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
+
+<style lang="scss" scoped>
+.mytree {
+  width: 100% !important;
+}
+
+.left {
+  .el-icon-office-building {
+    padding-left: 5px;
+    margin-right: 5px;
+  }
+  font-size: 14px;
+  font-weight: 700;
+}
+
+::v-deep .right {
+  display: flex;
+  text-align: right;
+  justify-content: space-around;
+  font-size: 14px;
+  font-weight: normal;
+  color: #000;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #000;
+}
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,76 @@
 <template>
-  <div>department</div>
+  <div class="departments">
+    <el-card class="card">
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="组织结构" name="first">
+          <div class="content">
+            <treeTool :item="topTree"></treeTool>
+            <hr />
+            <el-tree :data="treelist" :props="defaultProps" default-expand-all>
+              <template slot-scope="{ data }">
+                <treeTool :item="data"></treeTool>
+              </template>
+            </el-tree>
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {}
+import treeTool from './components/treetool.vue'
+import { getDepartmentsApi } from '../../api/departments'
+export default {
+  components: {
+    treeTool
+  },
+  created () {
+    getDepartmentsApi().then(res => {
+      this.rowList = res.depts
+      console.log(this.rowList)
+      this.treelist = this.hhh('')
+    })
+  },
+  data () {
+    return {
+      activeName: 'first',
+      topTree: {
+        name: '江苏传智播客教育科技股份有限公司',
+        isRoot: true
+      },
+      rowList: [],
+      treelist: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    hhh (str) {
+      const arr = []
+      this.rowList.forEach(item => {
+        if (item.pid == str) {
+          const children = this.hhh(item.id)
+          item.children = children
+          arr.push(item)
+        }
+      })
+      return arr
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.departments {
+  padding: 20px;
+  .card {
+    padding: 30px 20px 50px 50px;
+    .content {
+      padding: 20px 100px 0 120px;
+    }
+  }
+}
+</style>
